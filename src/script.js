fabba97387e4b603eb2051cfce4ada4ed5d1a363
Mars Adeniji <adenijifara@gmail.com>
2023-02-24 18:29:33 +0200

    update script.js

--- src/script.js
+++ src/script.js
@@ -1,16 +1,10 @@
 $(document).on("jt:toggled:multi", function (event, target) {
 	console.info($(target).parent().index());
 	if ($(target).parent().index() === 1) {
-		if ($("body").hasClass("theme3-body")) {
-			$("body").removeClass("theme3-body");
-		}
-		$("body").addClass("theme2-body");
+		$("#theme").attr("href", "dist/css/theme2.css");
 	} else if ($(target).parent().index() === 2) {
-		if ($("body").hasClass("theme2-body")) {
-			$("body").addClass("theme3-body");
-			$("body").removeClass("theme2-body");
-		}
+		$("#theme").attr("href", "dist/css/theme3.css");
 	} else if ($(target).parent().index() === 0) {
-		$("body").removeClass();
+		$("#theme").attr("href", "dist/css/style.css");
 	}
 });
