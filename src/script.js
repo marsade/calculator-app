7835f29bc34f8507fe599f6ec9e8569ff8d49f5e
Mars Adeniji <adenijifara@gmail.com>
2023-02-24 20:24:35 +0200

    update script.js

--- src/script.js
+++ src/script.js
@@ -1,3 +1,6 @@
+const result = $(".answer");
+
+// theme switcher function
 $(document).on("jt:toggled:multi", function (event, target) {
 	console.info($(target).parent().index());
 	if ($(target).parent().index() === 1) {
@@ -6,5 +9,27 @@ $(document).on("jt:toggled:multi", function (event, target) {
 		$("#theme").attr("href", "dist/css/theme3.css");
 	} else if ($(target).parent().index() === 0) {
 		$("#theme").attr("href", "dist/css/style.css");
+	}
+});
+
+let endValue = "";
+$("input").click(function () {
+	let input = $(this).attr("value");
+
+	// change x to *
+	if (input === "x") {
+		endValue += "*";
+	} else {
+		endValue += input;
+	}
+	result.text(endValue);
+	console.log(endValue);
+});
+
+$("#del-btn").click(function () {
+	endValue = endValue.substring(0, endValue.length - 1);
+	result.text(endValue);
+	if (endValue === "") {
+		result.text("0");
 	}
 });
